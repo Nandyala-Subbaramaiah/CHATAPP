3b186b05ad972541209e19a7748ed4aefd05279d
Nandyala-Subbaramaiah <subbaram2025.py@gmail.com>
2026-07-22 00:19:59 +0530

    Create react app for chat

---
+++ chat-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import ChatWindow from "./components/chat-window";
+
+function App() {
+  return (
+     <div>
+
+    <chat-window username="subbu"/>
+
+    </div>
+
+  );
+}
+
+export default App;
